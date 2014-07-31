bad35aa769f160ab3ee7be9fa78b9c647f612a31
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2014-07-31 19:50:17 +0200

    Updated functional test towards handle more users.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -33,6 +33,8 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Repair the bicycle')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Repair the bicycle')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -45,6 +47,35 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
         for row_text in ('1: Repair the bicycle', '2: Take a bicycle tour'):
             self.check_for_row_in_list_table(row_text)
+
+        # A new user Francis comes along the site
+        self.browser.quit()
+        ## We use a new browser session to make sure there is no
+        ## trace of Edith's through cokies or etc
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page
+        # There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Repair the bicycle', page_text)
+        self.assertNotIn('bicycle', page_text)
+
+        # Francis starts a new list
+        # There is no sign of Edith's list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Repair the bicycle', page_text)
+        self.assertIn('Buy milk', page_text)
+
         self.fail("Finish the test!")
 
     def check_for_row_in_list_table(self, row_text):
