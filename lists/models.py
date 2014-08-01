67b99752fa9712c914ec011149394c01cd085790
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2014-08-01 08:37:37 +0200

    Added: List model

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
     text = models.TextField(max_length=200)
+    list = models.ForeignKey(List)
--- lists/tests.py
+++ lists/tests.py
@@ -4,21 +4,28 @@ from django.template.loader import render_to_string
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         texts = ('The first (ever) list item', 'Item the second')
         for item_text in texts:
-            item = Item(text=item_text)
+            item = Item(text=item_text, list=list_)
             item.save()
 
+        saved_lists = List.objects.all()
+        self.assertEqual(saved_lists.count(), 1)
+        self.assertEqual(saved_lists[0], list_)
         items = Item.objects.all()
         self.assertEqual(items.count(), 2)
 
         for i in range(2):
             self.assertEqual(items[i].text, texts[i])
+            self.assertEqual(items[i].list, list_)
 
 
 class HomePageTest(TestCase):
@@ -61,9 +68,10 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
+        list_ = List.objects.create()
         item_texts = ('Item 1', 'Item 2')
         for i in item_texts:
-            Item.objects.create(text=i)
+            Item.objects.create(text=i, list=list_)
             response = self.client.get('/lists/the-only-list-in-the-world/')
         for i in item_texts:
             self.assertContains(response, i)
